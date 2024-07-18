cd6bb005b8f5591a2603a8c737f2c35fb41479c4
shubhamhugay <shubhamhugay@gmail.com>
2024-07-18 10:04:44 +0530

    update 3

---
+++ ReactProjects/ReactPractise/q1_starter/src/App.js
@@ -0,0 +1,21 @@
+import "./styles.css";
+import { useState } from "react";
+
+import { Navbar } from "./components/Navbar";
+import { Home } from "./pages/Home";
+import { List } from "./pages/List";
+import { Contact } from "./pages/Contact";
+
+
+export default function App() {
+ 
+
+
+  return (
+
+    <div className="App">
+      <Navbar />
+      
+    </div>
+  );
+}
